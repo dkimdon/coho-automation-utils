a3112711ecce9d261c2f0734220f9d7f96e26c10
David Kimdon <dkimdon@gmail.com>
2019-03-13 07:54:54 -0700

    refactor verification

--- preventative-maintenance/test.py
+++ preventative-maintenance/test.py
@@ -4,6 +4,10 @@ from periodic_job import select_tasks
 from datetime import datetime
 
 class TestPeriodicJob(unittest.TestCase):
+    def verify_output(self, actual, expected):
+        self.assertEqual(len(actual), len(expected))
+        for i in range(0, len(expected)):
+            self.assertDictEqual(actual[i], expected[i])
 
     def test_basic(self):
         rows = [[],
@@ -12,9 +16,7 @@ class TestPeriodicJob(unittest.TestCase):
                ]
         tasks = select_tasks(datetime(2018, 12, 1), rows)
         expected = [{'email': 'd@g.com', 'subject': 'this is a subject', 'body': 'do it' }]
-        self.assertEqual(len(expected), len(tasks))
-        for i in range(0, len(expected)):
-            self.assertDictEqual(expected[i], tasks[i])
+        self.verify_output(tasks, expected)
 
 
 if __name__ == '__main__':
