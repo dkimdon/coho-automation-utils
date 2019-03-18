5a9748109dc7e06d766861ab5e8e2aa3f85a2c4e
David Kimdon <dkimdon@gmail.com>
2019-03-17 20:14:24 -0700

    Add more tests.

--- preventative-maintenance/test.py
+++ preventative-maintenance/test.py
@@ -9,8 +9,11 @@ class TestPeriodicJob(unittest.TestCase):
         for i in range(0, len(expected)):
             self.assertDictEqual(actual[i], expected[i])
 
-    # task was done last year, needs to be done this year, too
-    def test_empty_done(self):
+    ################
+    # Yearly tasks #
+    ################
+
+    def test_task_was_done_this_year_and_needs_to_be_done_this_year_too(self):
         rows = [[],
                 ['subject', 'email',   'Year Interval', 'Month', 'done', 'body'],
                 ['sub',     'd@g.com', '1',             'dec',   'dec,2017',     'do it']
@@ -19,14 +22,44 @@ class TestPeriodicJob(unittest.TestCase):
         expected = [{'email': 'd@g.com', 'subject': 'sub', 'body': 'do it' }]
         self.verify_output(tasks, expected)
 
-    # This task was completed last year and doesn't need to be completed this year
-    def test_yearly_task_needs_to_be_done(self):
+    def test_task_was_done_this_year_already_and_it_does_not_need_to_be_done_again_yet(self):
+        rows = [[],
+                ['subject', 'email',   'Year Interval', 'Month', 'done',     'body'],
+                ['sub',     'd@g.com', '1',             'Dec',   'Dec,2018', 'do it']
+               ]
+        tasks = select_tasks(datetime(2018, 12, 1), rows)
+        expected = []
+        self.verify_output(tasks, expected)
+
+    def test_task_was_done_last_year_but_it_is_not_time_to_do_it_yet(self):
+        rows = [[],
+                ['subject', 'email',   'Year Interval', 'Month', 'done',     'body'],
+                ['sub',     'd@g.com', '1',             'Dec',   'June,2017', 'do it']
+               ]
+        tasks = select_tasks(datetime(2018, 5, 1), rows)
+        expected = []
+        self.verify_output(tasks, expected)
+
+    ###########################
+    # Multi-year period tasks #
+    ###########################
+
+    def test_task_completed_last_year_does_not_need_to_be_completed_this_year(self):
         rows = [[],
                 ['subject', 'email',   'Year Interval', 'Month', 'done',     'body'],
                 ['sub',     'd@g.com', '5',             'Dec',   'Dec,2017', 'do it']
                ]
         tasks = select_tasks(datetime(2018, 12, 1), rows)
         expected = []
+        self.verify_output(tasks, expected)
+
+    def test_task_was_done_3_years_ago_and_now_needs_to_be_done_again(self):
+        rows = [[],
+                ['subject', 'email',   'Year Interval', 'Month', 'done', 'body'],
+                ['sub',     'd@g.com', '3',             'dec',   'dec,2015',     'do it']
+               ]
+        tasks = select_tasks(datetime(2018, 12, 1), rows)
+        expected = [{'email': 'd@g.com', 'subject': 'sub', 'body': 'do it' }]
         self.verify_output(tasks, expected)
 
 
