ac92fcc070a0e2815bbda1efa11d3a7e87c57bce
David Kimdon <dkimdon@gmail.com>
2020-01-18 13:50:34 -0800

    Update to change directory and also log.

--- preventative-maintenance/periodic-job.sh
+++ preventative-maintenance/periodic-job.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 export PATH=/home/ubuntu/coho-automation-utils/preventative-maintenance/venv/bin:$PATH
 
+cd /home/ubuntu/coho-automation-utils/preventative-maintenance/
+
 git pull
-/home/ubuntu/coho-automation-utils/preventative-maintenance/periodic_job.py
-/home/ubuntu/coho-automation-utils/preventative-maintenance/periodic_job_gat.py
+/home/ubuntu/coho-automation-utils/preventative-maintenance/periodic_job.py | logger -t preventative-maintenance
+/home/ubuntu/coho-automation-utils/preventative-maintenance/periodic_job_gat.py | logger -t preventative-maintenance
